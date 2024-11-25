df147e0fbb1c6d2cd3fd75594fcd33a30c418569
bekmurod <bekmurod3388@gmail.com>
2024-11-25 18:57:02 +0500

    fix

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from pages import views
 
 urlpatterns = [
+    path("", views.home_uz, name="home"),
     path("uz", views.home_uz, name="home_uz"),
     path("ru", views.home_ru, name="home_ru"),
     path("en", views.home_en, name="home_en"),
