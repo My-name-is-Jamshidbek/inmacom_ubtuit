b644382daf96822e20b0e211ae6ee1f8dc533659
bekmurod <bekmurod3388@gmail.com>
2024-11-25 15:59:27 +0500

    first

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from pages import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("uz", views.home_uz, name="home_uz"),
+    path("ru", views.home_ru, name="home_ru"),
+    path("en", views.home_en, name="home_en"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
-def home(request):
-    return render(request, 'home.html')
+def home_ru(request):
+    return render(request, 'home_ru.html')
+
+def home_uz(request):
+    return render(request, 'home_uz.html')
+
+def home_en(request):
+    return render(request, 'home_en.html')
